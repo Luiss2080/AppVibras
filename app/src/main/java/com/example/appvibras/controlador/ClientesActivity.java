fd574ba3b3da58f6c7878393b5ae9a3e532970d2
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-01-15 08:07:26 -0400

    Add Intent import to ClientesActivity
    - Add `import android.content.Intent;` to `ClientesActivity.java`.

--- app/src/main/java/com/example/appvibras/controlador/ClientesActivity.java
+++ app/src/main/java/com/example/appvibras/controlador/ClientesActivity.java
@@ -1,6 +1,7 @@
 package com.example.appvibras.controlador;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
